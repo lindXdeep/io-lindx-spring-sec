62a8be8fa494a2b63760ff8604a0ff6c832d3756
lindx <linder.igor@gmail.com>
2021-03-21 05:32:55 +0300

    authenticated NoOpPasswordEncoder

--- src/main/java/io/lindx/sec/config/ConfigSecurity.java
+++ src/main/java/io/lindx/sec/config/ConfigSecurity.java
@@ -1,9 +1,14 @@
 package io.lindx.sec.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -11,12 +16,38 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http
-      .authorizeRequests()
-        .antMatchers("/users").authenticated();
+    
     
     http
+      .authorizeRequests()
+        
+        // делаем доступной только юзером сролью USER
+        .antMatchers("/users").hasAnyRole("USER")
+
+        // все отальные запросы доступны только авторизованным юзерам
+        .anyRequest().authenticated();
+    
+    // ннаша кастомная логин форма
+    http
       .formLogin()
-        .loginPage("/login");
+        .loginPage("/login").permitAll()
+        .loginProcessingUrl("/login/process")
+          .usernameParameter("email")
+          .passwordParameter("password");
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+      .inMemoryAuthentication()
+        .withUser("user@user")
+        .password("user")
+        .roles("USER");
+  }
+
+  // В данном примере отключаемшифрование
+  @Bean
+  public static NoOpPasswordEncoder passwordEncoder() {
+      return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
   }
 }
