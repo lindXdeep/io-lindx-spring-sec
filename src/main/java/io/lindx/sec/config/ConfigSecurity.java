3cf286af67fd839b6ad93f60950c99db693077a6
lindx <linder.igor@gmail.com>
2021-03-21 04:57:10 +0300

    loginPage and authenticated

--- src/main/java/io/lindx/sec/config/ConfigSecurity.java
+++ src/main/java/io/lindx/sec/config/ConfigSecurity.java
@@ -1,6 +1,7 @@
 package io.lindx.sec.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class ConfigSecurity extends WebSecurityConfigurerAdapter {
 
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .authorizeRequests()
+        .antMatchers("/users").authenticated();
+    
+    http
+      .formLogin()
+        .loginPage("/login");
+  }
 }
